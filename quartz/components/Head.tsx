7169e0b21ef9524fd5abd41b2d36759c4eb64177
DHintz137 <dhintz1@uwyo.edu>
2023-12-23 16:59:59 -0700

    Quartz sync: Dec 23, 2023, 4:59 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -31,6 +31,7 @@ export default (() => {
         <meta name="generator" content="Quartz" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href={fontStylePath} rel="stylesheet" type="text/css" spa-preserve />
         {css.map((href) => (
           <link key={href} href={href} rel="stylesheet" type="text/css" spa-preserve />
         ))}
