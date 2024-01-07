fa5f0df1e6062db1bcde09666f10367b97dcddf8
DHintz137 <dhintz1@uwyo.edu>
2024-01-07 16:45:38 -0700

    Quartz sync: Jan 7, 2024, 4:45 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,7 +7,7 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/DHintz137",
+      GitHub: "https://github.com/DHintz137/quartz",
       "LinkedIn": "https://www.linkedin.com/in/daniel-hintz-rstat/",
       "Email" : "dhintz1@uwyo.edu",
       "Resume": "https://github.com/DHintz137/quartz/blob/v4/content/Docs/Daniel_Hintz_resume.pdf"
